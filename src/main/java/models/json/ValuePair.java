2f164505be939d31820fddaad29d07db7c91b856
LSchultebraucks <L.Schultebraucks@t-online.de>
2019-02-28 23:35:08 +0100

    added json models, added constructor and getter/setter

--- src/main/java/models/json/ValuePair.java
+++ src/main/java/models/json/ValuePair.java
@@ -3,4 +3,25 @@ package models.json;
 public class ValuePair {
     private StringValue key;
     private Value value;
+
+    public ValuePair(StringValue key, Value value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public StringValue getKey() {
+        return key;
+    }
+
+    public void setKey(StringValue key) {
+        this.key = key;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
 }

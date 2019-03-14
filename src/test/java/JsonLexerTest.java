aea869b3813f23a6b3073a4e1aaf538f658e0a1c
Lasse Schultebraucks <l.schultebraucks@t-online.de>
2019-03-14 17:51:46 +0100

    JsonLexer: added tests for basic number lexing functionality

--- src/test/java/JsonLexerTest.java
+++ src/test/java/JsonLexerTest.java
@@ -1,5 +1,7 @@
 import models.json.lexer.JsonLexer;
+import models.json.lexer.Lexer;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -8,12 +10,19 @@ import java.util.List;
 
 public class JsonLexerTest {
 
+    private Lexer lexer;
+
+    @Before
+    public void initializeTests() {
+        lexer = new JsonLexer();
+    }
+
+
     @Test
     public void testJsonLexer_lex_ShouldLexSingleStringCorrectly() throws Exception {
         // Arrange
         String testString = "\"actual string\"";
         List<String> expected = Collections.singletonList("actual string");
-        JsonLexer lexer = new JsonLexer();
 
         // Act
         List<String> actual = lexer.lex(testString);
@@ -23,11 +32,88 @@ public class JsonLexerTest {
     }
 
     @Test
-    public void testJsonLexer_lex_Should_LexNumberCorrectly() throws Exception {
+    public void testJsonLexer_lex_ShouldLexIntegerNumberCorrectly() throws Exception {
         // Arrange
         String testString = "42";
         List<String> expected = Arrays.asList("42");
-        JsonLexer lexer = new JsonLexer();
+
+        // Act
+        List<String> actual = lexer.lex(testString);
+
+        // Assert
+        assertLexerResult(expected, actual);
+    }
+
+    @Test
+    public void testJsonLexer_lex_ShouldLexFloatNumberCorrectly() throws Exception {
+        // Arrange
+        String testString = "4.2";
+        List<String> expected = Arrays.asList("4.2");
+
+        // Act
+        List<String> actual = lexer.lex(testString);
+
+        // Assert
+        assertLexerResult(expected, actual);
+    }
+
+    @Test
+    public void testJsonLexer_lex_ShouldLexNegativeIntegerNumbersCorrectly() throws Exception {
+        // Arrange
+        String testString = "-42";
+        List<String> expected = Arrays.asList("-42");
+
+        // Act
+        List<String> actual = lexer.lex(testString);
+
+        // Assert
+        assertLexerResult(expected, actual);
+    }
+
+    @Test
+    public void testJsonLexer_lex_ShouldLexNegativeFloatNumbersCorrectly() throws Exception {
+        // Arrange
+        String testString = "-4.2";
+        List<String> expected = Arrays.asList("-4.2");
+
+        // Act
+        List<String> actual = lexer.lex(testString);
+
+        // Assert
+        assertLexerResult(expected, actual);
+    }
+
+    @Test
+    public void testJsonLexer_lex_ShouldLexNegativeFloatNumbersWithEpsilonCorrectly() throws Exception {
+        // Arrange
+        String testString = "-42e2";
+        List<String> expected = Arrays.asList("-42e2");
+
+        // Act
+        List<String> actual = lexer.lex(testString);
+
+        // Assert
+        assertLexerResult(expected, actual);
+    }
+
+    @Test
+    public void testJsonLexer_lex_ShouldLexNegativeNumbersCorrectly() throws Exception {
+        // Arrange
+        String testString = "-42";
+        List<String> expected = Arrays.asList("-42");
+
+        // Act
+        List<String> actual = lexer.lex(testString);
+
+        // Assert
+        assertLexerResult(expected, actual);
+    }
+
+    @Test
+    public void testJsonLexer_lex_ShouldLexStringAndNumberCorrectly() throws Exception {
+        // Arrange
+        String testString = "\"actual string\"42";
+        List<String> expected = Arrays.asList("actual string", "42");
 
         // Act
         List<String> actual = lexer.lex(testString);

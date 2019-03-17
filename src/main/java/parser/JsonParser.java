65c90115fa80a3b79491060c6a7d3edba7defa12
LSchultebraucks <L.Schultebraucks@t-online.de>
2019-03-17 20:52:11 +0100

    JsonParser: continued implementation

--- src/main/java/parser/JsonParser.java
+++ src/main/java/parser/JsonParser.java
@@ -7,11 +7,30 @@ import models.json.util.JsonObjectBuilder;
 import java.util.List;
 
 public class JsonParser implements Parser<JsonObject> {
+    private final Character LEFT_BRACE = '{';
+    private final Character RIGHT_BRACE = '}';
+    private final Character LEFT_BRACKET = '[';
+    private final Character RIGHT_BRACKET = ']';
+
     public JsonObject parse(List<Token> tokens) throws Exception {
         JsonObjectBuilder jsonObjectBuilder = new JsonObjectBuilder();
 
-        if ((Character) tokens.get(0).getTokenValue() != '{') {
-            throw new Exception("JSON object must be begin with '{', got: " + tokens.get(0).getTokenValue());
+        boolean isFirstToken = true;
+
+        while (tokens.size() > 0) {
+            Token currentToken = tokens.get(0);
+
+            if (isFirstToken && currentToken.getTokenValue() != LEFT_BRACE) {
+                throw new Exception("JSON object must be begin with '{', got: " + tokens.get(0).getTokenValue());
+            } else if (currentToken.getTokenValue() == LEFT_BRACKET) {
+                // Start of Array
+                continue;
+            } else if (currentToken.getTokenValue() == LEFT_BRACE) {
+                // Start of Object
+                continue;
+            }
+
+            isFirstToken = false;
         }
 
         return jsonObjectBuilder.build();
